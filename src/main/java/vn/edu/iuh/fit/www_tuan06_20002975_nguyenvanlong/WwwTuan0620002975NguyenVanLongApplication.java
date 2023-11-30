82afe87b68b270ba47f9ffa2233fa7fa57cd133c
NguyenLongMoon <longsky0912624119@gmail.com>
2023-12-01 02:46:48 +0700

    cap nhat post, postcomment, user(models)

---
+++ src/main/java/vn/edu/iuh/fit/www_tuan06_20002975_nguyenvanlong/WwwTuan0620002975NguyenVanLongApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.www_tuan06_20002975_nguyenvanlong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwTuan0620002975NguyenVanLongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwTuan0620002975NguyenVanLongApplication.class, args);
+    }
+
+}
---
+++ src/main/java/vn/edu/iuh/fit/www_tuan06_20002975_nguyenvanlong/backend/models/Post.java
@@ -0,0 +1,66 @@
+package vn.edu.iuh.fit.www_tuan06_20002975_nguyenvanlong.backend.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.Set;
+
+@Entity
+@Table(name = "post")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Post parent;
+    @Column(length = 75, nullable = false)
+    private String title;
+    @Column(name = "meta_title", length = 100)
+    private String metaTitle;
+    @Column(columnDefinition = "text")
+    private String summary;
+    @Column(nullable = false)
+    private Boolean published;
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+    @Column(name = "published_at")
+    private Instant publishedAt;
+    @Column(columnDefinition = "text")
+    private String content;
+
+    @OneToMany(mappedBy = "post")
+    @ToString.Exclude
+    private Set<PostComment> postComments;
+    @OneToMany(mappedBy = "parent")
+    @ToString.Exclude
+    private Set<Post> posts;
+
+    public Post(Long id) {
+        this.id = id;
+    }
+
+    public Post(User author, Post parent, String title, String metaTitle, String summary, Boolean published, Instant createdAt, Instant updatedAt, Instant publishedAt, String content) {
+        this.author = author;
+        this.parent = parent;
+        this.title = title;
+        this.metaTitle = metaTitle;
+        this.summary = summary;
+        this.published = published;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.publishedAt = publishedAt;
+        this.content = content;
+    }
+}
